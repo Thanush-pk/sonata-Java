393aa8d287435ed9891449dc558c742f8fa421be
Thanush <thanushpk16@gmail.com>
2022-06-16 16:27:25 +0530

    Create timesheetInterface.java

---
+++ timesheetInterface.java
@@ -0,0 +1,18 @@
+
+public interface timeSheetInterface {
+	public void createList() {
+		
+	}
+	public void addToList() {
+		
+	}
+	public void changeStatus() {
+}
+	public void getTimesheet() {
+		
+	}
+}
+class defaultTimeshetimpl implements timeSheetInterface{
+	//implements all the above method
+	
+}
